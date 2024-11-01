65e5e11162b5efca86822e3542aacc236a3416bb
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-11-01 16:23:33 -0300

    ci: :green_heart: Ajusta script de configuração do git

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,17 +3,17 @@
 configureGit() {
     git remote rm origin
     git remote add origin https://$1@github.com/leonardopn/test-release-it.git
-    git symbolic-ref HEAD refs/heads/main
+    git symbolic-ref HEAD refs/heads/$2
 
-    GITHUB_TOKEN=$1 yarn deploy:$2
+    GITHUB_TOKEN=$1 yarn deploy:$3
 }
 
 case $1 in
 master)
-    configureGit $2 prod
+    configureGit $2 $1 prod
     ;;
 beta)
-    configureGit $2 beta
+    configureGit $2 $1 beta
     ;;
 *)
     printf "Parâmetro inválido, uso correto: build [beta | main]" >&2
