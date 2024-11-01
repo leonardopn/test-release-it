fa0397822b09d6b4cf2c3fc2cfaf5b35c8b65bcf
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-11-01 12:58:31 -0300

    chore: :bug: Corrige o script de merge manter na beta
    Ecita a abertura do prompt de mensagem

--- scripts/mergeMainIntoBeta.sh
+++ scripts/mergeMainIntoBeta.sh
@@ -3,16 +3,12 @@
 # Verifica se o repositório está atualizado
 git fetch origin
 
+# Muda para a branch beta
+git checkout beta
+
 # Cria um merge da branch master na branch beta, resolvendo conflitos
 # de forma que as modificações da master sempre saiam na frente
-git checkout beta
-git merge --no-ff -X theirs master
-
-# Adiciona todas as alterações
-git add .
-
-# Cria um commit com a mensagem desejada
-git commit -m "Merge branch 'master' into beta [skip ci]" -n
+git merge --no-ff -X theirs master -m "Merge branch 'master' into beta [skip ci]"
 
 # Faz push para o repositório remoto
 git push origin beta
