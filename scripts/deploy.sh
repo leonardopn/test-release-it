a98077a5a0f25087a8e13fb74e94c0ed0e516200
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2024-11-01 16:00:12 -0300

    ci: :green_heart: Adiciona novo passo ao deploy
    Trata autenticação do git por ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+configureGit() {
+    git remote rm origin
+    git remote add origin https://$1@github.com/leonardopn/test-release-it.git
+    git symbolic-ref HEAD refs/heads/main
+
+    GITHUB_TOKEN=$1 yarn deploy:$2
+}
+
 case $1 in
 master)
-    GITHUB_TOKEN=$2 yarn deploy:prod
+    configureGit $2 prod
     ;;
 beta)
-    GITHUB_TOKEN=$2 yarn deploy:beta
+    configureGit $2 beta
     ;;
 *)
     printf "Parâmetro inválido, uso correto: build [beta | main]" >&2
